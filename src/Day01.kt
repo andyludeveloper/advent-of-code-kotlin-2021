cd7f2322ef50b75935c2a2360d355a0b75b9a517
Andy Lu <alu@futuredial.com>
2021-12-01 13:53:07 +0800

    [Day1] finish

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,47 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var lastItem = 0
+        var result = 0
+        input.forEach {
+            val value = it.toInt()
+            if (lastItem != 0 && value > lastItem) {
+                result++
+            }
+            lastItem = value
+
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var lastItem = 0
+        var result = 0
+
+        input.forEachIndexed { index, s ->
+            val first: Int
+            val second: Int
+            val third: Int
+
+            if (input.size-1 >= index + 2) {
+                first = input[index].toInt()
+                second = input[index + 1].toInt()
+                third = input[index + 2].toInt()
+                val value = first + second + third
+
+                if (lastItem != 0 && value > lastItem) {
+                    result++
+                }
+                lastItem = value
+            }
+        }
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
     println(part1(input))
     println(part2(input))
 }
