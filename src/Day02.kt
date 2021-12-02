31e3ee9fe462d7581076dcfe13d64de0bad7b201
Andy <andyludeveloper@gmail.com>
2021-12-02 23:32:50 +0800

    [Day02] fix file name incorrect

--- src/Day02.kt
+++ src/Day02.kt
@@ -44,11 +44,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day02_test")
     check(part1(testInput) == 150)
     check(part2(testInput) == 900)
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }
