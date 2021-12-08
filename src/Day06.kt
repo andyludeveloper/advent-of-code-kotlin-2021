3111a77828beb43fe41c5dd7b330341f56ebfcb8
Andy <andyludeveloper@gmail.com>
2021-12-08 22:53:26 +0800

    [Day06] Part 1 done

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,9 +1,27 @@
-
 fun main() {
 
     fun part1(input: List<String>): Int {
+        val initialList = input[0].split(",").map { it.toInt() }
+        var fishes = mutableListOf<Int>()
+        fishes.addAll(initialList)
+        var times = 0
+        while (times < 256) {
+            var add8 = 0
+            val nextDay = fishes.map {
+                if (it > 0) it - 1 else {
 
-        return input.size
+                    ++add8
+                    6
+                }
+            }.toMutableList()
+            for (add8Times in 0 until add8) {
+                nextDay.add(8)
+            }
+            println("times:$times, $nextDay")
+            fishes = nextDay
+            times++
+        }
+        return fishes.count()
     }
 
     fun part2(input: List<String>): Int {
@@ -12,9 +30,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 5)
-//    println(part2(testInput))
-//    check(part2(testInput) == 12)
+    println(part1(testInput))
+    check(part1(testInput) == 5934)
 
     val input = readInput("Day06")
     println(part1(input))
