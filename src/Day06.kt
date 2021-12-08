8e2fca6cb1b718f33d21d680c93a72d093b140c6
Andy <andyludeveloper@gmail.com>
2021-12-09 00:01:29 +0800

    [Day06] Use map rewrite part1, but part 2 out of memory

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,37 +1,60 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        val initialList = input[0].split(",").map { it.toInt() }
-        var fishes = mutableListOf<Int>()
-        fishes.addAll(initialList)
-        var times = 0
-        while (times < 256) {
-            var add8 = 0
-            val nextDay = fishes.map {
-                if (it > 0) it - 1 else {
+        val fishes = input[0]
+            .split(",")
+            .mapIndexed { index, it -> index to it.toLong() }
+            .toMap()
+            .toMutableMap()
 
+        var times = 0
+        while (times < 80) {
+            var add8 = 0
+            fishes.forEach { (index, value) ->
+                if (value > 0) fishes[index] = value -1 else {
                     ++add8
-                    6
+                    fishes[index] = 6
                 }
-            }.toMutableList()
-            for (add8Times in 0 until add8) {
-                nextDay.add(8)
             }
-            println("times:$times, $nextDay")
-            fishes = nextDay
+
+            for (add8Times in 0 until add8) {
+                fishes[fishes.size] = 8
+            }
             times++
         }
         return fishes.count()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val fishes = input[0]
+            .split(",")
+            .mapIndexed { index, it -> index to it.toLong() }
+            .toMap()
+            .toMutableMap()
+
+        var times = 0
+        while (times < 256) {
+            var add8 = 0
+            fishes.forEach { (index, value) ->
+                if (value > 0) fishes[index] = value -1 else {
+                    ++add8
+                    fishes[index] = 6
+                }
+            }
+
+            for (add8Times in 0 until add8) {
+                fishes[fishes.size] = 8
+            }
+            times++
+        }
+        return fishes.count().toLong()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
     println(part1(testInput))
     check(part1(testInput) == 5934)
+    check(part2(testInput) == 26984457539)
 
     val input = readInput("Day06")
     println(part1(input))
