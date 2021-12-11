416ae5ae5bc880984c40a0bafc8c0767d7c29631
Andy Lu <alu@futuredial.com>
2021-12-11 11:37:32 +0800

    [Day8] Init

---
+++ src/Day08.kt
@@ -0,0 +1,21 @@
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day08_test")
+    println(part1(testInput))
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
+
+    val input = readInput("Day08")
+    println(part1(input))
+    println(part2(input))
+}
