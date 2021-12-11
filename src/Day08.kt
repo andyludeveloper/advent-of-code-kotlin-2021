266934025607aca66c8921896e3b2e3ab6462e5b
Andy Lu <alu@futuredial.com>
2021-12-11 11:59:17 +0800

    [Day8] Part 1 done

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,8 +1,20 @@
-
 fun main() {
+    val digitMap = mapOf(
+        1 to 2,
+        4 to 4,
+        7 to 3,
+        8 to 7
+    )
 
     fun part1(input: List<String>): Int {
-        TODO()
+        return input.map { it.split(" | ")[1] }
+            .map { it.split(" ") }
+            .flatten()
+            .groupBy { it.count() }
+            .mapValues { it.value.size }
+            .filterKeys { digitMap.containsValue(it) }
+            .values
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -12,8 +24,8 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
     println(part1(testInput))
-    check(part1(testInput) == 37)
-    check(part2(testInput) == 168)
+    check(part1(testInput) == 26)
+//    check(part2(testInput) == 168)
 
     val input = readInput("Day08")
     println(part1(input))
